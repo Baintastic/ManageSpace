eebba4d619849f089839e00510e9468264facb9d
AzureAD\ThabaniTembe <thabani.tembe@firsttech.digital>
2021-12-07 19:57:32 +0200

    removed unsed code and fixed update staff member implentation

--- manage-space/src/app/staff-members/add-edit-staff-member/add-edit-staff-member.component.ts
+++ manage-space/src/app/staff-members/add-edit-staff-member/add-edit-staff-member.component.ts
@@ -5,6 +5,8 @@ import { StaffMember } from 'src/app/models/staff-member';
 import { Location } from '@angular/common';
 import { MemberService } from '../member.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { OfficeService } from 'src/app/offices/office.service';
+import { Office } from 'src/app/models/office';
 
 @Component({
   selector: 'app-add-edit-staff-member',
@@ -13,8 +15,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class AddEditStaffMemberComponent implements OnInit {
   @Input() staffmember?: any;
+  @Input() officeId!: string;
+  @Input() isAddMode: boolean = false;
 
-  isAddMode: boolean = true;
   memberForm: FormGroup = this.formBuilder.group({
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
@@ -26,12 +29,17 @@ export class AddEditStaffMemberComponent implements OnInit {
   currentTab: number = 0;
   memberId: string = '';
 
-  constructor(private formBuilder: FormBuilder, private location: Location, public activeModal: NgbActiveModal, private memberService: MemberService, private route: ActivatedRoute, private router: Router) { }
+  constructor(private formBuilder: FormBuilder,
+    private location: Location,
+    public activeModal: NgbActiveModal,
+    private memberService: MemberService,
+    private officeService: OfficeService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
-    if (this.staffmember === undefined) {
-      this.isAddMode = true;
-    } else {
+    if (!this.isAddMode) {
+      this.isAddMode = false;
       this.memberId = this.staffmember?.id!;
       this.memberService.getMemberbyId(this.memberId).subscribe(data => {
         this.selectedStaffMember = data.data() as StaffMember;
@@ -41,7 +49,7 @@ export class AddEditStaffMemberComponent implements OnInit {
         this.memberForm.get('avatar')?.setValue(this.selectedStaffMember?.avatar);
         this.selectedAvatar = this.selectedStaffMember?.avatar!;
       })
-    }
+    } 
     this.showTab(this.currentTab);
     this.avatars = ['orange', 'pink', 'orangered', 'brown', 'yellow', 'darkorchid', 'lightblue'];
   }
@@ -49,7 +57,6 @@ export class AddEditStaffMemberComponent implements OnInit {
   get f() { return this.memberForm?.controls; }
 
   onSubmit() {
-    console.log(this.memberForm)
     if (this.isAddMode) {
       this.addStaffMember();
     } else {
@@ -58,46 +65,64 @@ export class AddEditStaffMemberComponent implements OnInit {
   }
 
   addStaffMember(): void {
-    var member = this.getofficeFormvalues();
+    var member = this.getMemberFormvalues();
     this.memberService.createMember(member).then(() => {
       console.log('Created new member successfully!');
+
+      this.officeService.getOfficebyId(member.officeId).subscribe(data => {
+        var office = data.data() as Office;
+        var newMaxCapacity: number = +office.maxCapacity + 1;
+        office.maxCapacity = newMaxCapacity.toString();
+
+        this.officeService.updateOffice(member.officeId, office)
+          .then(() => console.log('Updated office details successfully!'))
+          .catch(err => console.log(err));
+        this.router.navigate(['/detail/', this.officeId]);
+      })
     });
   }
 
   updateStaffMember(): void {
-    var member = this.getofficeFormvalues();
-    if (this.selectedStaffMember) {
-      this.memberService.updateMember(this.memberId, member)
-        .then(() => console.log('Updated member details successfully!'))
-        .catch(err => console.log(err));
-    }
+    // var member = this.getMemberFormvalues();
+    // if (this.selectedStaffMember) {
+    //   this.memberService.updateMember(this.memberId, member)
+    //     .then(() => console.log('Updated member details successfully!'))
+    //     .catch(err => console.log(err));
+    // }
   }
 
   goBack(): void {
     this.location.back();
   }
 
-  getofficeFormvalues(): StaffMember {
+  getMemberFormvalues(): StaffMember {
     var office: StaffMember = {
       firstName: this.memberForm.value.firstName,
       lastName: this.memberForm.value.lastName,
-      officeId: this.staffmember.officeId,
+      officeId: this.officeId,
       avatar: this.memberForm.value.avatar,
     };
     return office;
   }
 
-  showTab(n: number) {
+  showTab(tabNumber: number): void {
     // This function will display the specified tab of the form ...
-    var tab = Array.from(document.getElementsByClassName('tab') as HTMLCollectionOf<HTMLElement>)
-    tab[n].style.display = "block";
+    var tabs = Array.from(document.getElementsByClassName('tab') as HTMLCollectionOf<HTMLElement>)
+    //for some reason , one of the tabs from the previous html modal gets picked up so we need to remove the first element
+    if(!this.isAddMode){
+      tabs[tabNumber].style.display = "block";
+    }
+    else{
+      tabs[tabNumber].style.display = "block";
+
+    }
 
     // ... and fix the Previous/Next buttons:
     var prevBtn = document.getElementById('prevBtn');
     var title = document.getElementById("title");
 
     if (prevBtn) {
-      if (n == 0) {
+      if (tabNumber == 0) {
         prevBtn.style.display = 'none';
         title?.classList.remove('col-10');
         title?.classList.add('col-12');
@@ -112,33 +137,34 @@ export class AddEditStaffMemberComponent implements OnInit {
     var nextBtn = document.getElementById('nextBtn');
     if (nextBtn) {
       var btnText = this.isAddMode ? 'ADD STAFF MEMBER' : 'UPDATE STAFF MEMBER';
-      nextBtn.innerHTML = (n == (tab.length - 1)) ? `${btnText}` : 'NEXT';
+      nextBtn.innerHTML = (tabNumber == (tabs.length - 1)) ? `${btnText}` : 'NEXT';
     }
 
     // ... and run a function that displays the correct step indicator:
-    this.fixStepIndicator(n)
+    this.fixStepIndicator(tabNumber)
   }
 
-  nextPrev(n: number) {
+  nextPrev(tabNumber: number): void {
+
     // This function will figure out which tab to display
-    var tab = Array.from(document.getElementsByClassName('tab') as HTMLCollectionOf<HTMLElement>)
+    var tabs = Array.from(document.getElementsByClassName('tab') as HTMLCollectionOf<HTMLElement>)
     // Exit the function if any field in the current tab is invalid:
     // if (n == 1 && !validateForm()) return false;
     // Hide the current tab:
-    tab[this.currentTab].style.display = 'none';
+    tabs[this.currentTab].style.display = 'none';
     // Increase or decrease the current tab by 1:
-    this.currentTab = this.currentTab + n;
+    this.currentTab = this.currentTab + tabNumber;
     // if you have reached the end of the form... :
-    if (this.currentTab + 1 > tab.length) {
-      this.onSubmit();
+    if (this.currentTab >= tabs.length) {
       this.activeModal.dismiss('Cross click')
+      this.onSubmit();
       return;
     }
     // Otherwise, display the correct tab:
     this.showTab(this.currentTab);
   }
 
-  fixStepIndicator(n: number) {
+  fixStepIndicator(n: number): void {
     // This function removes the "active" class of all steps...
     var i, step = document.getElementsByClassName('step');
     for (i = 0; i < step.length; i++) {
